25d37950916f7f1e1044dc72db4f64dc1df388aa
Dario Pasquini <dariopasquini@macbook-pro.home>
2026-04-03 14:50:46 +0200

    think tool

--- agentic_custom/agent/tooling/base_tools/__init__.py
+++ agentic_custom/agent/tooling/base_tools/__init__.py
@@ -1,1 +1,9 @@
-from .user_input import ask_user_for_input_tool
+from .user_input import ask_user_for_input_tool, ASK_USER_TOOL_NAME
+from .think import think_tool, THINK_TOOL_NAME
+
+__all__ = [
+    "ASK_USER_TOOL_NAME",
+    "THINK_TOOL_NAME",
+    "ask_user_for_input_tool",
+    "think_tool",
+]
